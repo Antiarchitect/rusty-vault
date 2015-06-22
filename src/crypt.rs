0f4438f2a38ff6d7bde05c01b2badb81603706d4
Andrey Voronkov <andrey.voronkov@medm.com>
2015-06-22 20:14:01 +0300

    Crypt moved to separate module.

---
+++ src/crypt.rs
@@ -0,0 +1,45 @@
+extern crate crypto;
+extern crate rand;
+
+use self::crypto::aes_gcm::AesGcm;
+use self::crypto::aead::AeadEncryptor;
+use self::crypto::aes::KeySize::KeySize256;
+use self::rand::{Rng, OsRng};
+
+const TAG_LENGTH: usize = 16;
+const KEY_LENGTH: usize = 32;
+const IV_LENGTH: usize = 12;
+
+pub struct AeadKey {
+  pub key: Box<[u8]>,
+  pub iv: Box<[u8]>
+}
+
+pub struct EncryptionResult {
+  pub key: AeadKey,
+  pub ciphertext: Box<Vec<u8>>,
+  pub tag: Box<[u8]>
+}
+
+pub fn encrypt(auth_data: &[u8], plaintext: &[u8]) -> EncryptionResult {
+
+	let plaintext_len: usize = plaintext.len();
+
+	let mut key: [u8; KEY_LENGTH] = [0; KEY_LENGTH];
+	let mut iv: [u8; IV_LENGTH] = [0; IV_LENGTH];
+	let mut rng = OsRng::new().ok().unwrap();
+	rng.fill_bytes(&mut key);
+	rng.fill_bytes(&mut iv);
+
+	let mut cipher = AesGcm::new(KeySize256, &key, &iv, &auth_data);
+
+ 	let mut ciphertext: Vec<u8> = Vec::with_capacity(plaintext_len);
+	unsafe { ciphertext.set_len(plaintext_len); }
+	let mut tag: [u8; TAG_LENGTH] = [0; TAG_LENGTH];
+
+	cipher.encrypt(plaintext, &mut ciphertext, &mut tag);
+
+	let aead_key = AeadKey { key: Box::new(key), iv: Box::new(iv) };
+
+	return EncryptionResult { key: aead_key, ciphertext: Box::new(ciphertext), tag: Box::new(tag) };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,13 @@
-extern crate crypto;
-extern crate rand;
-
-use crypto::aes_gcm::AesGcm;
-use crypto::aead::AeadEncryptor;
-use crypto::aes::KeySize::KeySize256;
-use rand::{Rng, OsRng};
+pub mod crypt;
 
 fn main() {
-    const TAG_LENGTH: usize = 16;
-    const KEY_LENGTH: usize = 32;
-    const IV_LENGTH: usize = 12;
     let external_id = "e338c3d0-855c-4103-b427-585148b9da34".to_string().into_bytes();
-    let data = "secret data".to_string().into_bytes();
+    let data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".to_string().into_bytes();
+
+    let result = crypt::encrypt(&external_id, &data);
     
-    let data_len = data.len();
-    
-    let mut key: [u8; KEY_LENGTH] = [0; KEY_LENGTH];
-    let mut iv: [u8; IV_LENGTH] = [0; IV_LENGTH];
-    let mut rng = OsRng::new().ok().unwrap();
-    rng.fill_bytes(&mut key);
-    rng.fill_bytes(&mut iv);
-    
-    let mut cipher = AesGcm::new(KeySize256, &key, &iv, &external_id);
-    
-    let mut out: Vec<u8> = std::iter::repeat(0).take(data_len).collect();
-    let mut tag: [u8; TAG_LENGTH] = [0; TAG_LENGTH];
-            
-    cipher.encrypt(&data, &mut out, &mut tag);
-    
-    println!("Data: {:?}", data);
-    println!("Cipher {:?}", out);
+    println!("Iv: {:?}", result.key.iv);
+    println!("Key: {:?}", result.key.key);
+    println!("Ciphertext: {:?}", result.ciphertext);
+    println!("Tag: {:?}", result.tag);
 }
