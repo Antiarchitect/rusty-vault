2a5ebe70e7dd2f2bf747c22a6f0df65fac2c5577
Andrey Voronkov <andrey.voronkov@medm.com>
2016-01-26 14:13:12 +0300

    Code compacted.

--- src/lib.rs
+++ src/lib.rs
@@ -39,40 +39,36 @@ pub fn dump(external_id: String, data: Vec<u8>) -> Result<(), String>  {
     let key_id = store_key(keys_path, StorableKey { key: result.key, iv: result.iv }).unwrap();
     let data_id = store_data(data_path, StorableData { ciphertext: result.ciphertext }).unwrap();
     let external_uuid = Uuid::parse_str(&external_id).unwrap();
-    store_map(maps_path, StorableMap { key_id: key_id, data_id: data_id, tag: result.tag })
+    store_map(maps_path, external_uuid, StorableMap { key_id: key_id, data_id: data_id, tag: result.tag })
+}
+
+fn store_json_string(path_prefix: String, path_key: Uuid, json: String) -> Result<(), String> {
+    let mut file = File::create(Path::new(&format!("{}/{}.json", path_prefix, path_key.to_string()))).ok().expect("Cannot create file");
+    match file.write_all(json.as_bytes()) {
+        Ok(_) => Ok(()),
+        Err(error) => Err(format!("Error: {}", error))
+    }
 }
 
 fn store_data(path_prefix: String, storable: StorableData) -> Result<Uuid, String> {
     let id = Uuid::new_v4();
-    let encoded_storable = json::encode(&storable).unwrap();
-
-    let mut file = File::create(Path::new(&format!("{}/{}.json", path_prefix, id.to_string()))).ok().expect("Cannot create file");
-    match file.write_all(encoded_storable.as_bytes()) {
-        Ok(_) => Ok(id),
-        Err(error) => Err(format!("Error: {}", error))
-    }
+    let json = json::encode(&storable).unwrap();
+    store_json_string(path_prefix, id, json).ok();
+    Ok(id)
 }
 
 fn store_key(path_prefix: String, storable: StorableKey) -> Result<Uuid, String> {
     let id = Uuid::new_v4();
-    let encoded_storable = json::encode(&storable).unwrap();
-
-    let mut file = File::create(Path::new(&format!("{}/{}.json", path_prefix, id.to_string()))).ok().expect("Cannot create file");
-    match file.write_all(encoded_storable.as_bytes()) {
-        Ok(_) => Ok(id),
-        Err(error) => Err(format!("Error: {}", error))
-    }
+    let json = json::encode(&storable).unwrap();
+    store_json_string(path_prefix, id, json).ok();
+    Ok(id)
 }
 
-fn store_map(path_prefix: String, storable: StorableMap) -> Result<(), String> {
-    let id = Uuid::new_v4();
-    let encoded_storable = json::encode(&storable).unwrap();
-
-    let mut file = File::create(Path::new(&format!("{}/{}.json", path_prefix, id.to_string()))).ok().expect("Cannot create file");
-    match file.write_all(encoded_storable.as_bytes()) {
-        Ok(_) => Ok(()),
-        Err(error) => Err(format!("Error: {}", error))
-    }
+fn store_map(path_prefix: String, external_id: Uuid, storable: StorableMap) -> Result<(), String> {
+    let id = external_id;
+    let json = json::encode(&storable).unwrap();
+    store_json_string(path_prefix, id, json).ok();
+    Ok(())
 }
 
 pub struct LoadResult {
