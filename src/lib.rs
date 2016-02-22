747bbdcdb0c1129c62c77f67808cba93ebb88dfb
Andrey Voronkov <voronkovaa@gmail.com>
2016-02-22 03:29:29 +0300

    Much more is handled by the storage module now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
 mod crypt;
 
-extern crate rustc_serialize;
-use rustc_serialize::json;
-
 extern crate uuid;
 use uuid::Uuid;
+
+extern crate rustc_serialize;
 
 use std::sync::mpsc::channel;
 use std::thread;
@@ -12,87 +11,44 @@ use std::thread;
 mod storages;
 use storages::filesystem as storage;
 
-const KEYS_PATH: &'static str = "/home/andrey/Documents/storages/keys";
-const DATA_PATH: &'static str = "/home/andrey/Documents/storages/data";
-const MAPS_PATH: &'static str = "/home/andrey/Documents/storages/maps";
-
-#[derive(RustcDecodable, RustcEncodable)]
-struct StorableKey {
-    key: Box<[u8]>,
-    iv: Box<[u8]>
-}
-
-#[derive(RustcDecodable, RustcEncodable)]
-struct StorableData {
-    ciphertext: Vec<u8>
-}
-
-#[derive(RustcDecodable, RustcEncodable)]
-struct StorableMap {
-    key_id: Uuid,
-    data_id: Uuid,
-    tag: Box<[u8]>
-}
-
 pub fn dump(external_id: String, data: Vec<u8>) -> Result<(), String>  {
     let result = crypt::encrypt(external_id.as_bytes(), &data);
 
     let (key_tx, key_rx) = channel();
-    let storable_key = StorableKey { key: result.key, iv: result.iv };
+    let storable_key = storage::StorableKey { key: result.key, iv: result.iv };
     thread::spawn(move || {
-        key_tx.send(store_key(storable_key).unwrap()).unwrap();
+        key_tx.send(storage::store_key(storable_key).unwrap()).unwrap();
     });
 
     let (data_tx, data_rx) = channel();
-    let storable_data = StorableData { ciphertext: result.ciphertext };
+    let storable_data = storage::StorableData { ciphertext: result.ciphertext };
     thread::spawn(move || {
-        data_tx.send(store_data(storable_data).unwrap()).unwrap();
+        data_tx.send(storage::store_data(storable_data).unwrap()).unwrap();
     });
 
     let key_id = key_rx.recv().unwrap();
     let data_id = data_rx.recv().unwrap();
     let external_uuid = Uuid::parse_str(&external_id).unwrap();
-    store_map(external_uuid, StorableMap { key_id: key_id, data_id: data_id, tag: result.tag })
-}
-
-fn store_data(storable: StorableData) -> Result<Uuid, String> {
-    let id = Uuid::new_v4();
-    let json = json::encode(&storable).unwrap();
-    storage::dump_json_string(DATA_PATH.to_string(), id, json).ok();
-    Ok(id)
-}
-
-fn store_key(storable: StorableKey) -> Result<Uuid, String> {
-    let id = Uuid::new_v4();
-    let json = json::encode(&storable).unwrap();
-    storage::dump_json_string(KEYS_PATH.to_string(), id, json).ok();
-    Ok(id)
-}
-
-fn store_map(external_id: Uuid, storable: StorableMap) -> Result<(), String> {
-    let id = external_id;
-    let json = json::encode(&storable).unwrap();
-    storage::dump_json_string(MAPS_PATH.to_string(), id, json).ok();
-    Ok(())
+    storage::store_map(external_uuid, storage::StorableMap { key_id: key_id, data_id: data_id, tag: result.tag })
 }
 
 pub fn load(external_id: &String) -> Vec<u8> {
-    let map: StorableMap = json::decode(&storage::load_json_string(&MAPS_PATH.to_string(), external_id).unwrap()).unwrap();
+    let map = storage::load_map(&external_id).unwrap();
 
     let (key_tx, key_rx) = channel();
     let key_id = map.key_id.to_string();
     thread::spawn(move || {
-        key_tx.send(json::decode(&storage::load_json_string(&KEYS_PATH.to_string(), &key_id).unwrap()).unwrap()).unwrap();
+        key_tx.send(storage::load_key(&key_id).unwrap()).unwrap();
     });
 
     let (data_tx, data_rx) = channel();
     let data_id = map.data_id.to_string();
     thread::spawn(move || {
-        data_tx.send(json::decode(&storage::load_json_string(&DATA_PATH.to_string(), &data_id).unwrap()).unwrap()).unwrap();
+        data_tx.send(storage::load_data(&data_id).unwrap()).unwrap();
     });
 
-    let key: StorableKey = key_rx.recv().unwrap();
-    let data: StorableData = data_rx.recv().unwrap();
+    let key = key_rx.recv().unwrap();
+    let data = data_rx.recv().unwrap();
 
     let result = crypt::decrypt(external_id.as_bytes(), &key.key, &key.iv, &data.ciphertext, &map.tag);
 
--- src/storages/filesystem.rs
+++ src/storages/filesystem.rs
@@ -4,6 +4,74 @@ use std::path;
 
 use uuid::Uuid;
 
+use rustc_serialize::json;
+
+const KEYS_PATH: &'static str = "/home/andrey/Documents/storages/keys";
+const DATA_PATH: &'static str = "/home/andrey/Documents/storages/data";
+const MAPS_PATH: &'static str = "/home/andrey/Documents/storages/maps";
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct StorableKey {
+    pub key: Box<[u8]>,
+    pub iv: Box<[u8]>
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct StorableData {
+    pub ciphertext: Vec<u8>
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct StorableMap {
+    pub key_id: Uuid,
+    pub data_id: Uuid,
+    pub tag: Box<[u8]>
+}
+
+
+pub fn store_data(storable: StorableData) -> Result<Uuid, String> {
+    let id = Uuid::new_v4();
+    let json = json::encode(&storable).unwrap();
+    dump_json_string(DATA_PATH.to_string(), id, json).ok();
+    Ok(id)
+}
+
+pub fn store_key(storable: StorableKey) -> Result<Uuid, String> {
+    let id = Uuid::new_v4();
+    let json = json::encode(&storable).unwrap();
+    dump_json_string(KEYS_PATH.to_string(), id, json).ok();
+    Ok(id)
+}
+
+pub fn store_map(external_id: Uuid, storable: StorableMap) -> Result<(), String> {
+    let id = external_id;
+    let json = json::encode(&storable).unwrap();
+    dump_json_string(MAPS_PATH.to_string(), id, json).ok();
+    Ok(())
+}
+
+pub fn load_data(data_id: &String) -> Result<StorableData, String> {
+    match json::decode(&load_json_string(&DATA_PATH.to_string(), &data_id).unwrap()) {
+        Ok(value) => Ok(value),
+        Err(error) => Err(format!("Error: {}", error))
+    }
+}
+
+pub fn load_key(key_id: &String) -> Result<StorableKey, String> {
+    match json::decode(&load_json_string(&KEYS_PATH.to_string(), &key_id).unwrap()) {
+        Ok(value) => Ok(value),
+        Err(error) => Err(format!("Error: {}", error))
+    }
+}
+
+pub fn load_map(external_id: &String) -> Result<StorableMap, String> {
+    match json::decode(&load_json_string(&MAPS_PATH.to_string(), &external_id).unwrap()) {
+        Ok(value) => Ok(value),
+        Err(error) => Err(format!("Error: {}", error))
+    }
+}
+
+
 fn construct_store_path(path_prefix: &String, path_key_string: &String) -> path::PathBuf {
     let mut path = path::PathBuf::from(path_prefix);
     path.push(path_key_string[0..2].to_string());
@@ -19,7 +87,7 @@ fn construct_storable_path(path_prefix: &path::PathBuf, path_key: &String) -> pa
     path
 }
 
-pub fn dump_json_string(path_prefix: String, path_key: Uuid, json: String) -> Result<(), String> {
+fn dump_json_string(path_prefix: String, path_key: Uuid, json: String) -> Result<(), String> {
     let store_path = construct_store_path(&path_prefix, &path_key.to_simple_string());
     fs::create_dir_all(&store_path).ok();
     let mut file = fs::File::create(construct_storable_path(&store_path, &path_key.to_string())).ok().expect("Cannot create file");
@@ -29,7 +97,7 @@ pub fn dump_json_string(path_prefix: String, path_key: Uuid, json: String) -> Re
     }
 }
 
-pub fn load_json_string(prefix: &String, id: &String) -> Result<String, String> {
+fn load_json_string(prefix: &String, id: &String) -> Result<String, String> {
     let path = construct_storable_path(&construct_store_path(prefix, id), id);
     let mut file = fs::File::open(&path).unwrap();
     let mut string = String::new();
