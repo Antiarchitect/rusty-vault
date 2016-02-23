dde81fd3ce998148ccdad42f68e0e88385c05306
Andrey Voronkov <voronkovaa@gmail.com>
2016-02-24 00:04:34 +0300

    On a road to refactoring.

--- src/storages/filesystem.rs
+++ src/storages/filesystem.rs
@@ -10,6 +10,70 @@ const KEYS_PATH: &'static str = "/home/andrey/Documents/storages/keys";
 const DATA_PATH: &'static str = "/home/andrey/Documents/storages/data";
 const MAPS_PATH: &'static str = "/home/andrey/Documents/storages/maps";
 
+struct Storage {
+    path: String
+}
+
+trait FilesystemStorage {
+
+    fn new(path: String) -> Self;
+
+    fn construct_store_path(path_prefix: &String, path_key_string: &String) -> path::PathBuf {
+        let mut path = path::PathBuf::from(path_prefix);
+        path.push(path_key_string[0..2].to_string());
+        path.push(path_key_string[2..4].to_string());
+        path.push(path_key_string[4..6].to_string());
+        path
+    }
+
+    fn construct_storable_path(path_prefix: &path::PathBuf, path_key: &String) -> path::PathBuf {
+        let mut path = path::PathBuf::from(path_prefix);
+        path.push(path_key);
+        path.set_extension("json");
+        path
+    }
+
+    fn dump_json(&self, path_key: &String, json: String) -> Result<(), String> {
+        let store_path = self.construct_store_path(&self.path, path_key);
+        fs::create_dir_all(&store_path).ok();
+        let mut file = fs::File::create(self.construct_storable_path(&store_path, path_key)).ok().expect("Cannot create file");
+        match file.write_all(json.as_bytes()) {
+            Ok(_) => Ok(()),
+            Err(error) => Err(format!("Error: {}", error))
+        }
+    }
+}
+
+trait DataStorage: FilesystemStorage {
+    fn dump(id: String, storable: StorableData) -> Result<(), String>;
+    fn load(id: String) -> Result<StorableData, String>;
+}
+
+trait KeysStorage: FilesystemStorage {
+    fn dump(id: String, storable: StorableKey) -> Result<(), String>;
+    fn load(id: String) -> Result<StorableKey, String>;
+}
+
+trait MapsStorage: FilesystemStorage {
+    fn dump(id: String, storable: StorableMap) -> Result<(), String>;
+    fn load(id: String) -> Result<StorableMap, String>;
+}
+
+impl FilesystemStorage for Storage {
+    fn new(path: String) -> Storage {
+        Storage { path: path }
+    }
+}
+
+impl DataStorage for FilesystemStorage {
+    fn dump(&self, id: String, storable: StorableData) -> Result<(), String> {
+        self.dump_json(&id, json::encode(&storable).unwrap())
+    }
+    fn load(id: String) -> Result<StorableData, String> {
+
+    }
+}
+
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct StorableKey {
     pub key: Box<[u8]>,
@@ -26,28 +90,6 @@ pub struct StorableMap {
     pub key_id: Uuid,
     pub data_id: Uuid,
     pub tag: Box<[u8]>
-}
-
-
-pub fn store_data(storable: StorableData) -> Result<Uuid, String> {
-    let id = Uuid::new_v4();
-    let json = json::encode(&storable).unwrap();
-    dump_json_string(DATA_PATH.to_string(), id, json).ok();
-    Ok(id)
-}
-
-pub fn store_key(storable: StorableKey) -> Result<Uuid, String> {
-    let id = Uuid::new_v4();
-    let json = json::encode(&storable).unwrap();
-    dump_json_string(KEYS_PATH.to_string(), id, json).ok();
-    Ok(id)
-}
-
-pub fn store_map(external_id: Uuid, storable: StorableMap) -> Result<(), String> {
-    let id = external_id;
-    let json = json::encode(&storable).unwrap();
-    dump_json_string(MAPS_PATH.to_string(), id, json).ok();
-    Ok(())
 }
 
 pub fn load_data(data_id: &String) -> Result<StorableData, String> {
@@ -67,42 +109,6 @@ pub fn load_key(key_id: &String) -> Result<StorableKey, String> {
 pub fn load_map(external_id: &String) -> Result<StorableMap, String> {
     match json::decode(&load_json_string(&MAPS_PATH.to_string(), &external_id).unwrap()) {
         Ok(value) => Ok(value),
-        Err(error) => Err(format!("Error: {}", error))
-    }
-}
-
-
-fn construct_store_path(path_prefix: &String, path_key_string: &String) -> path::PathBuf {
-    let mut path = path::PathBuf::from(path_prefix);
-    path.push(path_key_string[0..2].to_string());
-    path.push(path_key_string[2..4].to_string());
-    path.push(path_key_string[4..6].to_string());
-    path
-}
-
-fn construct_storable_path(path_prefix: &path::PathBuf, path_key: &String) -> path::PathBuf {
-    let mut path = path::PathBuf::from(path_prefix);
-    path.push(path_key);
-    path.set_extension("json");
-    path
-}
-
-fn dump_json_string(path_prefix: String, path_key: Uuid, json: String) -> Result<(), String> {
-    let store_path = construct_store_path(&path_prefix, &path_key.to_simple_string());
-    fs::create_dir_all(&store_path).ok();
-    let mut file = fs::File::create(construct_storable_path(&store_path, &path_key.to_string())).ok().expect("Cannot create file");
-    match file.write_all(json.as_bytes()) {
-        Ok(_) => Ok(()),
-        Err(error) => Err(format!("Error: {}", error))
-    }
-}
-
-fn load_json_string(prefix: &String, id: &String) -> Result<String, String> {
-    let path = construct_storable_path(&construct_store_path(prefix, id), id);
-    let mut file = fs::File::open(&path).unwrap();
-    let mut string = String::new();
-    match file.read_to_string(&mut string) {
-        Ok(_) => Ok(string),
         Err(error) => Err(format!("Error: {}", error))
     }
 }
